e94094eb14af447da13fd16d8111358a3291916a
Bruno Araujo <bruno.araujo@sou.unifeob.edu.br>
2021-10-19 00:14:31 -0300

    Tipos de variaveis

---
+++ types-of-variables/type-string.js
@@ -0,0 +1,16 @@
+const texto1 = "Olá Mundo";
+const texto2 = 'Olá Mundo';
+const senha = "senhasSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é ";
+console.log(citacao);
+
+// template string OU template literal
+const templateString = `Template String Top`;
+
+// concatenação (+)
+
+const meuNome = "Bruno Araujo";
+
+console.log(citacao + meuNome);

418a3cd9bfe8cbf41b6a8331ccfbbd2468b6fe24
Bruno Araujo <bruno.araujo@sou.unifeob.edu.br>
2021-10-19 00:53:32 -0300

    Conversões de tipos de variáveis.

---
+++ conversions.js
@@ -0,0 +1,16 @@
+// Tipo de dado
+// booleanos
+
+// Conversão implicita
+const numero = 456;
+const numeroSting = "456";
+
+console.log(numero == numeroSting) // true
+
+console.log(numero + numeroSting) //456456
+
+// Conversao explicita
+//Number()
+//String()
+
+console.log(numero + Number(numeroSting)) //912

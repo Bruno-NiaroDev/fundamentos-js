6641a834087de7c5ff5d7534c89fa7756b6780d4
Bruno Araujo <bruno.araujo@sou.unifeob.edu.br>
2021-10-26 01:07:53 -0300

    Entendendo alguns comparadores.

---
+++ operadores/comparadores.js
@@ -0,0 +1,16 @@
+// == (comparação implicita)
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero == texto);
+
+// == só compara o valor
+// === compara o valor e tipo de dado
+
+// conversão explicita
+Number()
+String()
+
+// documentação do tipos de operadores em JS
+// https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Expressions_and_operators
